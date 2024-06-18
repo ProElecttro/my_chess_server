e4e181797c0f4ff08e6b8a96ac095a42fd2815bf
Purushottam <131853935+ProElecttro@users.noreply.github.com>
2024-06-19 04:48:01 +0530

    modify your server setup to listen on all network interfaces (0.0.0.0):

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
 // src/app.ts
+
 import "reflect-metadata";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import AppDataSource from "./ormconfig";
-import { handleSocketEvents } from "./socketHandlers";
+import handleSocketEvents from "./socketHandlers";
 
 const app = express();
-const port = 3002;
+const port = 5000;
 const httpServer = createServer(app);
 const io = new Server(httpServer);
-
-
 
 AppDataSource.initialize()
   .then(() => {
     console.log('Database connected successfully');
     handleSocketEvents(io);
-    httpServer.listen(port, () => {
+    httpServer.listen(port, '0.0.0.0', () => {
       console.log(`Server running on http://localhost:${port}`);
     });
+    
   })
   .catch((error) => {
     console.error('Database connection error', error);
